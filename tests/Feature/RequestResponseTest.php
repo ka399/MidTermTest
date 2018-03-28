5ebb760fb573fc898e0b9ae5e84f4df9e398cb5d
kashish <ka399@njit.edu>
2018-03-28 18:27:43 -0400

    Commit 10: Test2: Added feature test function to check if the login page returns a 200 status code.Test runs successfully.

--- tests/Feature/RequestResponseTest.php
+++ tests/Feature/RequestResponseTest.php
@@ -9,13 +9,25 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class RequestResponseTest extends TestCase
 {
     /**
-     * Test 1.  Feature test to check if the register page return a 200 status code.
+     * Test 1.  Feature test to check if the register page returns a 200 status code.
      *
      * @return void
      */
     public function testRegisterPage()
     {
         $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test 2.  Feature test to check if the login page returns a 200 status code.
+     *
+     * @return void
+     */
+    public function testLoginPage()
+    {
+        $response = $this->get('/login');
 
         $response->assertStatus(200);
     }
