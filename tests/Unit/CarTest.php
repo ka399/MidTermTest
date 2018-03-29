e420c2b6e88d2b788ffd48f3b48300c21d368fea
kashish <ka399@njit.edu>
2018-03-29 00:34:15 -0400

    Commit 18: Test10: Created a unit test to update the year of a car record to 2000.Test runs successfully.

--- tests/Unit/CarTest.php
+++ tests/Unit/CarTest.php
@@ -11,7 +11,7 @@ class CarTest extends TestCase
 {
     /**
      * Test 9.  Unit test to insert new record to Cars Table in the database.
-     * New Car Record :
+     * New Car Record : Honda Accord 2011
      * @return void
      */
     public function testInsertCar()
@@ -27,6 +27,27 @@ class CarTest extends TestCase
 
         //Year
         $car-> year = 2011;
+
+        //test if the new user is saved in the database
+        $this->assertTrue($car->save());
+    }
+
+    /**
+     * Test 10.  Unit test to update the year of a car to 2000.
+     * I am going to update the recently inserted car record : Honda Accord 2011 to Honda Accord 2000.
+     * @return void
+     */
+    public function testUpdateCar()
+    {
+        //get the instance for a car whose name has to be updated
+        $car = Car::where('make', 'Honda')
+                  ->where('model', 'Accord')
+                  ->where('year', 2011)
+                   ->first();
+
+
+        //Update the name to 'Steve Smith'
+        $car->year= 2000;
 
         //test if the new user is saved in the database
         $this->assertTrue($car->save());
