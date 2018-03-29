1ac399c6e82c335a37f2df8bb764eba25db6d3de
kashish <ka399@njit.edu>
2018-03-29 00:10:21 -0400

    Commit 17: Test9: Created a unit test for inserting a new record in cars table.Test runs successfully.

---
+++ tests/Unit/CarTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Car;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CarTest extends TestCase
+{
+    /**
+     * Test 9.  Unit test to insert new record to Cars Table in the database.
+     * New Car Record :
+     * @return void
+     */
+    public function testInsertCar()
+    {
+        //create a new instance of Car
+        $car = new Car();
+
+        //Make
+        $car-> make = 'Honda';
+
+        //Model
+        $car-> model = 'Accord';
+
+        //Year
+        $car-> year = 2011;
+
+        //test if the new user is saved in the database
+        $this->assertTrue($car->save());
+    }
+}
